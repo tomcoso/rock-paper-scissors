13c1e8c93f5a8f661abc739ff001d1fc27ee18bc
tomcoso <tomasdessy0@gmail.com>
2022-04-05 18:38:31 +0100

    finish script, comparison by numbers

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-values = ['Rock', 'Paper', 'Scissors'];
+const values = ['Rock', 'Paper', 'Scissors'];
 let userValue = null;
 let computerValue = null;
 let userScore = 0;
@@ -10,31 +10,42 @@ function getValue() {
 }
 console.log(getValue());
 
-function runGame(userValue) {
+function runGame(val) {
     computerValue = getValue();
-    switch (userValue) {
+    switch (val) {
         case 0 :
             if (computerValue == 2) {
-                outcome = 'User wins this round!';
-                userScore =+ 1;
+                outcome = `User wins this round!\n${values[userValue]} beats ${values[computerValue]}.`;
+                userScore += 1;
                 break;
             }
         case 1 :
             if (computerValue == 0) {
-                outcome = 'User wins this round!';
-                userScore =+ 1;
+                outcome = `User wins this round!\n${values[userValue]} beats ${values[computerValue]}.`;
+                userScore += 1;
                 break;
             }
         case 2 :
             if (computerValue == 1) {
-                outcome = 'User wins this round!';
-                userScore =+ 1;
+                outcome = `User wins this round!\n${values[userValue]} beats ${values[computerValue]}.`;
+                userScore += 1;
                 break;
             }
         default : 
-            outcome = 'User loses this round!';
+            outcome = `User loses this round!\n${values[computerValue]} beats ${values[userValue]}.`;
             computerScore += 1;
     }
     return outcome;
 }
 
+for (i=0 ; i < 5 ; i++) {
+    userValue = parseInt(prompt('Select one to play:\n0 for Rock\n1 for Paper\n2 for Scissors'));
+    console.log(`You picked ${values[userValue]}`);
+    console.log(runGame(userValue));
+    console.log(`Scores\nUser ${userScore}\nComputer ${computerScore}`)
+}
+if (userScore > computerScore) {
+    console.log('User wins the game!')
+} else if (userScore < computerScore) {
+    console.log('Bad luck! Computer wins this time!')
+} else {console.log("It's a tie! What a day!")}
