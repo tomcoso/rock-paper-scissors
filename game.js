57bf3cc8e2818f206ee632d0cadb1b1bbae1c4ca
tomcoso <tomasdessy0@gmail.com>
2022-04-13 17:39:14 +0100

    Finish UI integration. add reload button

--- game.js
+++ game.js
@@ -4,12 +4,31 @@ let computerValue = null;
 let userScore = 0;
 let computerScore = 0;
 let outcome = null;
+let terminate = null;
 
 function getValue() {
     return values[Math.floor(Math.random()*3)];
 }
 
+// Because removeEventListener wouldn't work with anonymous functions
+function checkWinner() {
+    if (userScore === 5) {
+        scoretext.innerText = 'Game over!'
+        header.innerText = 'Player Wins!'
+        terminate = 1;
+        return;
+    }else if(computerScore === 5) {
+        scoretext.innerText = 'Game over!'
+        header.innerText = 'Computer wins!'
+        terminate = 1;
+        return;
+    }
+}
+
 function runGame(val) {
+    if (terminate) { 
+        return;
+    }
     computerValue = getValue();
     switch (true) {
         case val == computerValue:
@@ -33,39 +52,26 @@ function runGame(val) {
             break;
     }
     scoretext.innerText = outcome;
+    htmlPlayerScore.innerText = userScore;
+    htmlComputerScore.innerText = computerScore;
+    playerSelection.innerText = val.toUpperCase();
+    computerSelection.innerText = computerValue.toUpperCase();
+    checkWinner();
 }
-
-function main() {
-    for (i=0 ; i < 5 ; i++) {
-    userValue = (prompt('Select Rock, Paper, or Scissors to play!')).toLowerCase();
-
-    if (userValue != 'rock' && userValue != 'paper' && userValue != 'scissors') {
-        console.log('Bad input, start over!');
-        return;
-    }
-
-    console.log(runGame(userValue));
-    console.log(`Scores  U: ${userScore}  //  C: ${computerScore}`)
-    }
-    if (userScore > computerScore) {
-        console.log('User wins the game!')
-    } else if (userScore < computerScore) {
-        console.log('Bad luck! Computer wins this time!')
-    } else {console.log("It's a tie! What a day!")}
-}
-//main()
 
 const scoretext = document.querySelector('#scoretext');
 const htmlPlayerScore = document.querySelector('#playerscore');
 const htmlComputerScore = document.querySelector('#computerscore');
 const buttons = document.querySelectorAll('.playerselection');
+const playerSelection = document.querySelector('#player');
+const computerSelection = document.querySelector('#computer');
+const restartBtn = document.querySelector('#restart');
+const header = document.querySelector('#header');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         runGame(button.id);
-        htmlPlayerScore.innerText = userScore;
-        htmlComputerScore.innerText = computerScore;
     })
 })
 
-
+restartBtn.addEventListener('click', () => {location.reload()});
